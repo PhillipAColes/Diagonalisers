a8b5f2c1521e532bb7d6ec8a7c253dd66a0bb2b4
Phillip Coles <dc-cole3@login-e-11.data.cluster>
2019-11-07 17:19:57 +0000

    removed ELPA directory

--- ELPA/diag-login-run.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-module load rhel7/default-peta4
-module load intel/bundles/complib/2019.3
-module load elpa/2019.05.001-intel-17.0.4
-mpirun -np 4 -l nice -19 diag__elpa.x > diag-elpa.out
